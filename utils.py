6504ee75cd6e6a7d1f9aa421487893e7948fc439
w1adek <wkwaszuk@gmail.com>
2026-05-28 12:01:17 +0200

    add docstrings

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 # przykladowe funkcje utils.py
 
 def add(a: int, b: int) -> int:
+    """Addition"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Substraction"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Multiplication"""
     return a * b
 
 
 def divide (a: int, b: int) -> float:
+    """Dividing"""
     return a / b
